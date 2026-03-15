6e4dbaf55b7d9c4e463c5711519762540df05b6d
shayanfaghani <10611014@polimi.it>
2026-03-15 23:57:02 +0000

    fix: replace string spread with Array.from for TS downlevel compat

--- lib/notifications.ts
+++ lib/notifications.ts
@@ -2,7 +2,7 @@ function urlBase64ToUint8Array(base64String: string): Uint8Array {
     const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
     const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
     const rawData = window.atob(base64);
-    return Uint8Array.from([...rawData].map((c) => c.charCodeAt(0)));
+    return Uint8Array.from(Array.from(rawData).map((c) => c.charCodeAt(0)));
 }
 
 export async function requestPermission(): Promise<NotificationPermission> {
